61cec4b89fcd638de3a09b4d4502a782d21be949
MirandaPedro15 <mirandairiarte.pedro15@gmail.com>
2025-11-04 21:29:19 -0500

    arreglo prueba exitosa

--- sum.test.js
+++ sum.test.js
@@ -1,1 +1,1 @@
-const sum = require('./sum'); test('suma 4 + 5 = 3', () => { expect(sum(4, 5)).toBe(3); });
+const sum = require('./sum'); test('suma 4 + 5 = 9', () => { expect(sum(4, 5)).toBe(9); });
